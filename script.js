bd0b8b239d28fca4b5a5cdb17d551ed0c405b9f2
MarekLansky <lany8.rr@gmail.com>
2025-11-11 10:47:10 +0100

    f6

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 getText("text.txt");
 
 async function getText(file) {
-  let myObject = await fetch(file);
-  let myText = await myObject.text();
-  document.getElementById("demo").innerHTML = myText;
+  const res = await fetch(file);
+  const text = await res.text();
+
+  // rozdělíme text podle našich oddělovačů
+  const parts = parseSections(text);
+
+  // a vložíme jednotlivé části na stránku
+  document.getElementById("aktuality").innerHTML = parts.#AKTUALITY;
+  document.getElementById("souteze").innerHTML = parts.SOUTEZE;
+
+}
+
+function parseSections(text) {
+  const sections = {};
+  const regex = /#(\w+)\s*([\s\S]*?)(?=#\w+|$)/g;
+  let match;
+  while ((match = regex.exec(text)) !== null) {
+    const key = match[1].trim();
+    const value = match[2].trim();
+    sections[key] = value;
+  }
+  return sections;
 }
